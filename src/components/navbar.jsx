49ebcd6884b485117610ca0d2cf3ce5f88c29246
Neauto <sthasandip014@gmail.com>
2024-07-18 12:46:57 +0545

    navbar added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,82 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import NavLink from "./navLink";
+
+const links = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg: px-20 xl:px-48">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className="md:hidden lg:flex">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">Nextjs</span>
+          <span className="w-10 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+      {/* SOCIAL */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="/">
+          <img src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <img src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <img src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
+      <div className="md:hidden">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
