4338babe2d30e7a0d41adf2cea36c60b9508300d
Neauto <sthasandip014@gmail.com>
2024-07-20 11:32:56 +0545

    Home page updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,7 +25,7 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg: px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
@@ -33,13 +33,13 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
           <span className="text-white mr-1">Nextjs</span>
-          <span className="w-10 h-8 rounded bg-white text-black flex items-center justify-center">
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
             .dev
           </span>
         </Link>
