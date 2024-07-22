e8799ad78af017597e24b6ae8d0e35517c8761fe
Neauto <sthasandip014@gmail.com>
2024-07-22 18:11:25 +0545

    added tailwind css in homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,17 +8,20 @@ const Homepage = () => {
         <Image src="/hero.png" alt="" fill className="object-contain" />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 items-center justify-center">
         {/* TITLE */}
-        <h1 className="text-2xl md:text-4xl font-bold">
+        <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">
           Crafting Digital Experiences, Desinging Tomorrow
         </h1>
         {/* DESCRIPTION */}
         <p className="md:text-xl">
           Welcome to my digital canvas, where innovation and creativity meet.
+          With a keen eye for aesthetics and a mastery of code, my portfolio
+          showcase a diverse collection of projects that reflect my commitment
+          to excellence.
         </p>
         {/* BUTTONS */}
-        <div className="flex gap-4">
+        <div className="w-full flex gap-4">
           <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
             View My Work
           </button>
